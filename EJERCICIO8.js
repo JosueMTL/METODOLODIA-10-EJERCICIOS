ab33a8e751b634e20644cff0c6e3895f09d2b7a4
JosueMTL <jdmatailo@sudamericano.edu.ec>
2023-01-27 08:54:45 -0500

    commit

--- EJERCICIO8.js
+++ EJERCICIO8.js
@@ -1,7 +1,7 @@
 // 6. **Ejercicio 08**
     
 //     Invertir el orden de una cadena de texto almacenada en una array, , considerar que el tamaño de la cadena es un numero ***impar*`world**=[’s’,’o’,’n’,’a’,’c’,’n’,’e’,’u’,’c’]`
-
+    
 let  ​​nums  =  [ 5 , 6 , 7 , 8 ] ;
 let  ​​tamaño  =  nums . longitud ;
 /*let i = (tamaño-1);*/
